2212e14a0620ca435c8b1fcfa52bc5b60497483d
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 07:59:32 +0800

    pseudocode

---
+++ remove_leading_spaces.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael CLouiee A.
+# 3-12-25
+# batch 5, Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
+
+# ask user input full name
+# strip unnecessary spaces before the name
+# print stripped output
+# end
