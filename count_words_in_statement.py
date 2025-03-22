0f1ee86321c9fe11e8723e67fd3ba261659c4964
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:30:12 +0800

    pseudocode

---
+++ count_words_in_statement.py
@@ -0,0 +1,9 @@
+# Inducil, Raphael Clouiee A.
+# 3-22-25
+# batch 5, prog07: create a program that ask the user to input a complete statement. print the number of words in the input.
+
+# ask user to input a complete statement
+# split the statement into words
+# count the number of words
+# print output
+# end
