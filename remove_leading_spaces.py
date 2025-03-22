4873511b8c3fda1155a9d124acb399934f3eedef
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:03:23 +0800

    user input, strip leading spaces. code complete

--- remove_leading_spaces.py
+++ remove_leading_spaces.py
@@ -3,6 +3,12 @@
 # batch 5, Prog01: Create a program that ask the user to input their fullname with several space characters at the beginning. Print the input without the spaces in the beginning.
 
 # ask user input full name
+
+full_name = input("Enter your full name with spaces at the beginning: ")
+
 # strip unnecessary spaces before the name
 # print stripped output
+
+print(full_name.strip())
+
 # end
