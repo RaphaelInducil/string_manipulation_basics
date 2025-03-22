f87e8a332012af365a1b98f04dd026abe90becc6
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:33:20 +0800

    user input, split input, count input, print output. code complete

--- count_words_in_statement.py
+++ count_words_in_statement.py
@@ -3,7 +3,16 @@
 # batch 5, prog07: create a program that ask the user to input a complete statement. print the number of words in the input.
 
 # ask user to input a complete statement
+
+statement = input("Enter a complete statement: ")
+
 # split the statement into words
 # count the number of words
+
+word_count = len(statement.split())
+
 # print output
+
+print(word_count)
+
 # end
