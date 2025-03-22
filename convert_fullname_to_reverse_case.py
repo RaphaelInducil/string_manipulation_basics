61d947c4d6081a575d3639b1072883197d98609b
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:26:53 +0800

    pseudocode

---
+++ convert_fullname_to_reverse_case.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael Clouiee A.
+# 3-22-25
+# batch 5, prog06: create a program that ask the user to input their fullname in incorrect casing. print each character of the input in reverse casing.
+
+# ask user to input their full name in incorrect casing
+# convert each character to reverse casing
+# print output
+# end
