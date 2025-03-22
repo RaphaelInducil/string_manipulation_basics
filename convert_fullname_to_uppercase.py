0c072e40f9373f2386b53b5494c69f02c7b1a065
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:18:57 +0800

    user input, change input to upper. code complete

--- convert_fullname_to_uppercase.py
+++ convert_fullname_to_uppercase.py
@@ -3,6 +3,12 @@
 # batch 5, Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
 
 # ask user to input their full name
+
+full_name = input("Enter your full name: ")
+
 # convert input to uppercase
 # print output
+
+print(full_name.upper())
+
 # end
