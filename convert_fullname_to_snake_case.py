af93569172e19585e3d84938f1a6f15dc47a21ba
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:40:14 +0800

    pseudocode

---
+++ convert_fullname_to_snake_case.py
@@ -0,0 +1,9 @@
+# Inducil, Raphael Clouiee A.
+# 3-22-25
+# batch 5, prog10: create a program that ask the user to input their fullname in incorrect casing. print the input in snake case.
+
+# ask user to input their full name in incorrect casing
+# convert input to lowercase
+# replace spaces with underscores
+# print output
+# end
