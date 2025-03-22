2edb99599f4504bf4e5d56f1fbb773ae6e0d8c22
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:21:22 +0800

    pseudocode

---
+++ convert_fullname_to_lowercase.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael Clouiee A.
+# 3-22-25
+# batch 5, prog04: create a program that ask the user to input their fullname. print the input in all lower case.
+
+# ask user to input their full name
+# convert input to lowercase
+# print output
+# end
