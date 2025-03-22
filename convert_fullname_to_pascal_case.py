1eb0f24cceaf9edda97673eecbc041ce3c6af183
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:38:45 +0800

    user input, format input to title & pascal, print output. code complete

--- convert_fullname_to_pascal_case.py
+++ convert_fullname_to_pascal_case.py
@@ -3,6 +3,15 @@
 # batch 5, prog09: create a program that ask the user to input their fullname in incorrect casing. print the input in pascal case.
 
 # ask user to input their full name in incorrect casing
+
+full_name = input("Enter your full name: ")
+
 # convert input to pascal case (capitalize first letter of each word and remove spaces)
+
+pascal_case_name = full_name.title().replace(" ", "")
+
 # print output
+
+print(pascal_case_name)
+
 # end
