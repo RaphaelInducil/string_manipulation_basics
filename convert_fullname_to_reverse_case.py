62254d8e4057b6bf80ad5eb729cab46e46f8237f
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:28:14 +0800

    user input, change input to swapcase format. code complete

--- convert_fullname_to_reverse_case.py
+++ convert_fullname_to_reverse_case.py
@@ -3,6 +3,12 @@
 # batch 5, prog06: create a program that ask the user to input their fullname in incorrect casing. print each character of the input in reverse casing.
 
 # ask user to input their full name in incorrect casing
+
+full_name = input("Enter your full name: ")
+
 # convert each character to reverse casing
 # print output
+
+print(full_name.swapcase())
+
 # end
