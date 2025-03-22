53729553952fcd212016f4cd89b2e63e4c608d88
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:34:22 +0800

    pseudocode

---
+++ count_characters_in_fullname.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael Clouiee A.
+# 3-22-25
+# batch 5, prog08: create a program that ask the user to input their fullname. print the number of characters in the input.
+
+# ask user to input their full name
+# count the number of characters (including spaces)
+# print output
+# end
