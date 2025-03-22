f0b1e8c0d229371e2bb9bc5d7fa5a242814f265b
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:35:46 +0800

    user input, coount words in input, print count output. code complete

--- count_characters_in_fullname.py
+++ count_characters_in_fullname.py
@@ -3,6 +3,15 @@
 # batch 5, prog08: create a program that ask the user to input their fullname. print the number of characters in the input.
 
 # ask user to input their full name
+
+full_name = input("Enter your full name: ")
+
 # count the number of characters (including spaces)
+
+char_count = len(full_name)
+
 # print output
+
+print(char_count)
+
 # end
