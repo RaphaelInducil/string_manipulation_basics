b51fd2d48b4cea46fc53683e4cee34cacb439ed0
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:22:14 +0800

    user input, change input to lower. code complete

--- convert_fullname_to_lowercase.py
+++ convert_fullname_to_lowercase.py
@@ -3,6 +3,12 @@
 # batch 5, prog04: create a program that ask the user to input their fullname. print the input in all lower case.
 
 # ask user to input their full name
+
+full_name = input("Enter your full name: ")
+
 # convert input to lowercase
 # print output
+
+print(full_name.lower())
+
 # end
