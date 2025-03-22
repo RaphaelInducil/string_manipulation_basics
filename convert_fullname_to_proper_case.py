f1149dd321e47ec1246a1caa2716bdfa7de649ee
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:25:17 +0800

    user input, change input to title format. code complete

--- convert_fullname_to_proper_case.py
+++ convert_fullname_to_proper_case.py
@@ -3,6 +3,12 @@
 # batch 5, prog05: create a program that ask the user to input their fullname in incorrect casing. print the input in proper casing.
 
 # ask user to input their full name in incorrect casing
+
+full_name = input("Enter your full name: ")
+
 # convert input to proper casing
 # print output
+
+print(full_name.title())
+
 # end
