d5243697a22c65fe3cd45f3772b3293a19cc3115
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:36:57 +0800

    pseudocode

---
+++ convert_fullname_to_pascal_case.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael Clouiee A.
+# 3-22-25
+# batch 5, prog09: create a program that ask the user to input their fullname in incorrect casing. print the input in pascal case.
+
+# ask user to input their full name in incorrect casing
+# convert input to pascal case (capitalize first letter of each word and remove spaces)
+# print output
+# end
