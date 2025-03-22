e7e98e9f66e02aa2c53bb406b5e90bb73760b4b6
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:17:41 +0800

    pseudocode

---
+++ convert_fullname_to_uppercase.py
@@ -0,0 +1,8 @@
+# Inducil, Raphael Clouiee A.
+# 3-22-25
+# batch 5, Prog03: Create a program that ask the user to input their fullname. Print the input in all capital letter.
+
+# ask user to input their full name
+# convert input to uppercase
+# print output
+# end
