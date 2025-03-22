39c3923da352fb022c0f0e87ca3569900159a342
Raphael Clouiee A. Inducil <clouieeraphael@gmail.com>
2025-03-22 08:10:00 +0800

    user input, fill spaces with zero (limit of 6),  print output. code complete

--- format_number_with_leading_zeros.py
+++ format_number_with_leading_zeros.py
@@ -2,8 +2,14 @@
 # 3-22-25
 # batch 5, Prog02: Create a program that ask the user to input a number (0-1000). Print the number in 6 digit format. Add zeros at the beginning to complete the 6 digit.
 
-# Ask user input 1-1000
+# Ask user input 0-1000
+
+number = int(input("Enter a number (0-1000): "))
+
 # convert extra spaces (limit of 6) to 0 (on left end)
 # limit number to 6 digits
 # print output
+
+print(f"{number:06}")
+
 # end
